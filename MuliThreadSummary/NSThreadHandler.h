21c7e8c6f5bf2751e982e6bf06379fdce48bb737
tenyears <comexie@yahoo.com>
2018-09-09 12:10:17 +0800

    Initial Project and add nsthread

---
+++ MuliThreadSummary/NSThreadHandler.h
@@ -0,0 +1,13 @@
+//
+//  NSThreadHandler.h
+//  MuliThreadSummary
+//
+//  Created by UncleDrew on 2018/9/9.
+//  Copyright © 2018年 UncleDrew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSThreadHandler : NSObject
+
+@end
---
+++ MuliThreadSummary/NSThreadHandler.m
@@ -0,0 +1,13 @@
+//
+//  NSThreadHandler.m
+//  MuliThreadSummary
+//
+//  Created by UncleDrew on 2018/9/9.
+//  Copyright © 2018年 UncleDrew. All rights reserved.
+//
+
+#import "NSThreadHandler.h"
+
+@implementation NSThreadHandler
+
+@end
