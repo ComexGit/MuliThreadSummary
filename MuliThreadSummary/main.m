9e4e27e5aeb11946d6153329b32afd22542eea65
tenyears <comexie@yahoo.com>
2018-09-09 10:12:58 +0800

    Initial Commit

---
+++ MuliThreadSummary/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MuliThreadSummary
+//
+//  Created by UncleDrew on 2018/9/9.
+//  Copyright © 2018年 UncleDrew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
