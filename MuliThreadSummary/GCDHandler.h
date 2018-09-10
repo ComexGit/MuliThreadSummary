9d20b453bd968c5692fed1235b57857954946e78
tenyears <comexie@yahoo.com>
2018-09-10 09:12:39 +0800

    add GCD

---
+++ MuliThreadSummary/GCDHandler.h
@@ -0,0 +1,13 @@
+//
+//  GCDHandler.h
+//  MuliThreadSummary
+//
+//  Created by yuqian on 2018/9/10.
+//  Copyright © 2018年 UncleDrew. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GCDHandler : NSObject
+
+@end
---
+++ MuliThreadSummary/GCDHandler.m
@@ -0,0 +1,13 @@
+//
+//  GCDHandler.m
+//  MuliThreadSummary
+//
+//  Created by yuqian on 2018/9/10.
+//  Copyright © 2018年 UncleDrew. All rights reserved.
+//
+
+#import "GCDHandler.h"
+
+@implementation GCDHandler
+
+@end
